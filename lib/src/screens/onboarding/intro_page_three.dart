a4339f57c819954adf5d1c522e5c7d45f990ac8c
ilham.fathoni <ilham.fathoni>
2023-09-14 12:49:50 +0700

    fix ui on boarding test

--- lib/src/screens/onboarding/intro_page_three.dart
+++ lib/src/screens/onboarding/intro_page_three.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:lottie/lottie.dart';
+// import 'package:lottie/lottie.dart';
 
 class IntroPageThree extends StatelessWidget {
   const IntroPageThree({super.key});
@@ -8,8 +8,8 @@ class IntroPageThree extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: const Color(0xFF220D46),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 30.0),
+      child: const Padding(
+        padding: EdgeInsets.symmetric(horizontal: 30.0),
         child: Stack(
           alignment: Alignment.center,
           children: [
@@ -19,14 +19,13 @@ class IntroPageThree extends StatelessWidget {
               crossAxisAlignment:
                   CrossAxisAlignment.center, // Menengahkan secara horizontal
               children: [
-                // Lottie animation
-                Lottie.network(
-                  'https://lottie.host/2fdc7ada-cdab-4f5c-b9b9-f322be0a91c8/cVB3qAgJOr.json',
-                  height: 200, // Sesuaikan tinggi animasi sesuai kebutuhan Anda
-                  width: 200, // Sesuaikan lebar animasi sesuai kebutuhan Anda
-                ),
+                // Lottie.network(
+                //   'https://lottie.host/2fdc7ada-cdab-4f5c-b9b9-f322be0a91c8/cVB3qAgJOr.json',
+                //   height: 200, // Sesuaikan tinggi animasi sesuai kebutuhan Anda
+                //   width: 200, // Sesuaikan lebar animasi sesuai kebutuhan Anda
+                // ),
                 // Teks pertama
-                const Padding(
+                Padding(
                   padding: EdgeInsets.only(bottom: 20.0),
                   child: Text(
                     'Find Your Character Match with Your DNA',
@@ -38,7 +37,7 @@ class IntroPageThree extends StatelessWidget {
                   ),
                 ),
                 // Teks kedua
-                const Text(
+                Text(
                   'Lorem ipsum dolor sit amet consectetur. Amet',
                   style: TextStyle(
                     fontSize: 16,
